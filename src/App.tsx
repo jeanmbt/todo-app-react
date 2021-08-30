8717f0696074e843632d803bba26faa01037db8e
Jean Michel Battirola <jeanbattirola@gmail.com>
2021-08-30 15:22:30 +0200

    feat(TODO1): add hard coded main components and some style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Title, Page, TaskList, TaskItem } from "./styles/styles"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page>
+      <Title>Today</Title>
+      
+      <input type='text' value="add new task"></input>
+      <TaskList>
+        
+        <TaskItem>
+          <input type="checkbox" id="task" name="task"/>
+          <label htmlFor="task">task task task </label>
+        </TaskItem>
+        <TaskItem>
+          <input type="checkbox" id="task" name="task"/>
+          <label htmlFor="task">task task task </label>
+        </TaskItem>
+
+      </TaskList>
+    </Page>
   );
 }
 
