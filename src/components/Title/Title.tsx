b55bf6f43488d606547ee1b6f5798fd17d198b9d
Jean Michel Battirola <66276446+jeanmbt@users.noreply.github.com>
2021-12-20 17:39:45 +0100

    Todo 20 (#14)
    * Todo 2.1 - Fix architecture, fix styling using styled components, add TS typing, React Router and required packages
    
    * feat(TODO1): Add architecture folders
    
    * feat(TODO1): add hard coded main components and some style.
    
    * feat(TODO1): Create template and basic components
    
    * feat(TODO2): Add 'add todo' feature
    
    * git: prepare branch for rebase
    
    * fix(architecture): add files and exports/imports to components folders
    
    * refactor: remove comments and unnecessary code.
    
    * package install: add react-contenteditable for title component
    
    * fix(names): fix names of components following DDD
    
    * feat(TODO-2): Install React Icons package
    
    * feat(TODO-2): add icons instead of input for checking todo
    
    * refactor(TODO-2): remove commented code and dependecies for unnecessary component 'CheckBox'
    
    * feat(TODO-2): add themes.ts, refactor styles for TodoForm
    
    * todo-2(feat): add (TS) typing for components
    
    * fix(TODO-2): Remove unnecessary components, resolve half of gh reviews
    
    * Feat(TODO-2): Add Typing for components
    
    * Feat(TODO-2): Add Typing for components
    
    * refactor(TODO-2): Replace global functions to utils, make directories for types (TODO: Check types, export and replace in components file)
    
    * refactor(TODO-2): Add Types to types directory and add typing for missing states and props
    
    * refactor(TODO-2): Add Types to types directory and add typing for missing states and props
    
    * core(TODO-2): Install and apply React Router
    
    * core(TODO-2): remove unwanted packages and code
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * deploy(main): put main to production at gh pages
    
    * Update README.md
    
    * TODO-10(feat): Todo text shows on edit
    
    * fix(TODO-Deploy): accept multiple paths for homepage
    
    * fix(TODO-Deploy): accept multiple paths for homepage
    
    * Merge dev to main
    
    * fix(TODO-Deploy): remove unnecessary Hashrouter
    
    * refactor(fix-id): set Todo Id as it's index
    
    * fix(TODO-12): fix ID property of ToDo
    
    * feat(TODO-20): add content editable function on list Title
    
    * feat(TODO-20): add content editable function on list Title
    
    * feat(TODO-20): add content editable function on list Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { StyledTitle } from './Title.style'
+import ContentEditable from 'react-contenteditable'
 
-function Title() {
-  return <StyledTitle />
+const Title = () => {
+  const html = useRef('')
+
+  const handleChange = (e) => {
+    html.current = e.target.value
+  }
+
+  return (
+    <StyledTitle>
+      <ContentEditable
+        html={(html.current = 'Click to add a list title')}
+        onChange={handleChange}
+      ></ContentEditable>
+    </StyledTitle>
+  )
 }
 
 export default Title
