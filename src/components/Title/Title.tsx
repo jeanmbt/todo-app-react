02a5e49b02600733750eb908751507b729110ddd
Jean Michel Battirola <jeanbattirola@gmail.com>
2021-12-27 19:22:58 +0100

    feat(storedata): add title to local storage

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,22 +1,33 @@
-import React, { useRef } from 'react'
+import React, { useContext, useRef } from 'react'
 import { StyledTitle } from './Title.style'
 import ContentEditable from 'react-contenteditable'
 import usePersistedState from '../../utils/usePersistedState'
+import { ThemeContext } from 'styled-components'
 
 const Title = () => {
-  const html = useRef('')
-  const ref = html.current
-  const [text, setText] = usePersistedState<any>('listTitle', ref)
+  const { colors } = useContext(ThemeContext)
+  const html = useRef('Click to add a list title')
+  const [text, setText] = usePersistedState<any>('title', html)
+
   const handleChange = (e) => {
     html.current = e.target.value
+    setText(html.current)
   }
 
   return (
     <StyledTitle>
-      <ContentEditable
-        html={(html.current = 'Click to add a list title')}
-        onChange={handleChange}
-      ></ContentEditable>
+      <div
+        style={{
+          borderBottom: `1px solid ${colors.secondary}`,
+          minWidth: '2em',
+          maxWidth: '80vw',
+        }}
+      >
+        <ContentEditable
+          html={(html.current = text)}
+          onChange={handleChange}
+        ></ContentEditable>
+      </div>
     </StyledTitle>
   )
 }
